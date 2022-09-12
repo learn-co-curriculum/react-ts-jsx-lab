1bdca52424e817f4348a40164b21700a43c97b1f
Jerrica Bobadilla <jbobadilla27@gmail.com>
2022-09-12 13:04:57 -0700

    initialize files

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { name, city } from "../data/data.js";
+
+function Home() {
+  // update the JSX being returned!
+  return <div>Home</div>;
+}
+
+export default Home;
